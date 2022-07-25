9881f4a6d3b1efd00971a7f07ad1eec4f526c812
GoYoshino <go.yoshino.2@gmail.com>
2022-07-25 16:25:04 +0900

    removed unused base method

--- core/serialized_object_array.py
+++ core/serialized_object_array.py
@@ -28,18 +28,6 @@ class SerializedObjectArray(SerializedObject):
         assert index < len(self.__items), f"index out of bounds: {index} for Array[{len(self.__items)}]"
         return self.__items[index]
 
-    @staticmethod
-    def from_stream(stream: BinaryIO, count: int, type_info: List[BinaryType]):
-        assert len(type_info) == count
-        items = []
-        for i in range(count):
-            if type_info[i] == BinaryType.String:
-                items.append(LengthPrefixedString.from_stream(stream))
-            else:
-                raise Exception(f"not implemented for {type_info[i]}")
-
-        return SerializedObjectArray(items)
-
     def __repr__(self):
         string = "["
         for item in self.__items:
