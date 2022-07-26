d308ddc3dcde51c239f26601bf857ba6414523fc
Go Yoshino <go.yoshino.2@gmail.com>
2022-07-27 06:48:15 +0900

    JSON吐き出し機能を実装。.k/.itemについては出力できるようになった。 コードと設計がきたないのでマージする前になんとかすること

--- underrail_translation_kit/json_/generate_jsons.py
+++ underrail_translation_kit/json_/generate_jsons.py
@@ -1,35 +1,26 @@
-import fnmatch
+import json
 import os
 from pathlib import Path
-from typing import List, Tuple, Dict
+from typing import List, Dict
 
-from underrail_translation_kit.json_.annotation import add_annotation
 from underrail_translation_kit.unpacker import unpack_as_stream
 from underrail_translation_kit.msnrbf_parser import ParseResult, parse_binary_stream
+from underrail_translation_kit.json_.postprocess import postprocess
+from underrail_translation_kit.util import list_all_files, get_relative_path_from_data
 
 # now constant for development. will be command line in future
-UNDERRAIL_DATA_DIR = r"D:\SteamLibrary\steamapps\common\Underrail\backup\data"
-JSON_OUT_DIR = "D:/underrail_json"
+UNDERRAIL_DATA_DIR = Path(r"G:\SteamLibrary\steamapps\common\Underrail\data")
+JSON_OUT_DIR = Path("G:/underrail_json/data")
 
 TARGETS = [
     "*.k",
     "*.item"
 ]
 
-def list_all_files(data_dir: str) -> List[Tuple[str, str]]:
-    file_list = []
-    for root, dirs, files in os.walk(data_dir, topdown=False):
-        for name in files:
-            for target in TARGETS:
-                fn = os.path.join(root, name)
-                if fnmatch.fnmatch(fn, target):
-                    file_list.append((root, name))
-
-    return file_list
-
-def generate_json_dictionary(target_path: str, original_text: str, description: str) -> Dict[str, str]:
+def generate_json_dictionary(class_name: str, object_id: int, original_text: str, description: str) -> Dict[str, str]:
     return {
-        "targetPath": target_path,
+        "className": class_name,
+        "objectId": object_id,
         "description": description,
         "originalText": original_text,
         "translatedText": ""
@@ -38,15 +29,17 @@ def generate_json_dictionary(target_path: str, original_text: str, description: 
 def generate_whole_extract_json(obj: ParseResult) -> List[Dict[str, str]]:
     json_dict_list = []
 
-    classes = obj.get_all_member_class()
+    classes = obj.get_member_class_dict()
     for key in classes:
         class_ = classes[key]
         members = class_.get_string_member_dict()
         for member_key in members:
+            class_name = class_.get_name()
+
             member = members[member_key]
-            path = f"{key}.{member_key}"
-            json_dict = generate_json_dictionary(path, member.get_string(), "not available (whole extraction)")
-            add_annotation(json_dict)
+            object_id = member.get_object_id()
+
+            json_dict = generate_json_dictionary(class_name, object_id, member.get_string(), "n/a")
 
             json_dict_list.append(json_dict)
 
@@ -56,18 +49,11 @@ def load_object(filepath: str) -> ParseResult:
     with unpack_as_stream(filepath) as f:
         return parse_binary_stream(f)
 
-def get_relative_path_from_data(path: Path) -> Path:
-    index_of_data = -1
-    for i, part in enumerate(path.parts):
-        if part == "data":
-            index_of_data = i
-            break
-    return Path("/".join(path.parts[index_of_data:]))
 
 if __name__ == "__main__":
     os.sep = "/"
 
-    all_files = list_all_files(UNDERRAIL_DATA_DIR)
+    all_files = list_all_files(UNDERRAIL_DATA_DIR, TARGETS)
 
     for file in all_files:
         path = Path(file[0] + "/" + file[1])
@@ -78,5 +64,10 @@ if __name__ == "__main__":
         os.makedirs(save_dir, exist_ok=True)
 
         obj = load_object(path)
-        json = generate_whole_extract_json(obj)
-        print(json)
+        json_data = generate_whole_extract_json(obj)
+        postprocess(json_data)
+
+        save_path = str(JSON_OUT_DIR.joinpath(relative_path)) + ".json"
+        print(save_path)
+        with open(save_path, "w") as f:
+            json.dump(json_data, f, indent=4)
