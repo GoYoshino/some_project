f67771f4b1e6bd8d510ab19e67ddfcd0dd06466c
Go Yoshino <go.yoshino.2@gmail.com>
2022-07-27 10:01:46 +0900

    utilを更新

--- underrail_translation_kit/util/path_util.py
+++ underrail_translation_kit/util/path_util.py
@@ -3,14 +3,14 @@ import os
 from pathlib import Path
 from typing import List, Tuple
 
-def list_all_files(data_dir: str, targets: List[str]) -> List[Tuple[str, str]]:
+def list_all_files(data_dir: str, targets: List[str]) -> List[str]:
     file_list = []
     for root, dirs, files in os.walk(data_dir, topdown=False):
         for name in files:
             for target in targets:
                 fn = os.path.join(root, name)
                 if fnmatch(fn, target):
-                    file_list.append((root, name))
+                    file_list.append(fn)
 
     return file_list
 
