39788936f184de129e03d910c18501b58ea1bb5d
Go Yoshino <go.yoshino.2@gmail.com>
2022-07-27 18:45:58 +0900

    Added problematic file tests. It tests all files that caused problem on parser, for pseudo coverage. Though this is not unit tests they are fairly runs fast, so we will always run them

--- tests/msnrbf_parser/helper/bytes_asserts.py
+++ tests/msnrbf_parser/helper/bytes_asserts.py
@@ -7,3 +7,8 @@ def assertEqualToStream(test_class, raw_bytes: bytes, stream: BinaryIO):
     stream.seek(0)
     expected_raw_bytes = stream.read()
     test_class.assertEqual(raw_bytes, expected_raw_bytes)
+
+def assertEqualToStreamWithoutHeader(test_class, raw_bytes: bytes, stream: BinaryIO):
+    stream.seek(0)
+    expected_raw_bytes = stream.read()
+    test_class.assertEqual(raw_bytes, expected_raw_bytes)
