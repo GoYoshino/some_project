1bf90c15c7e26e32c08baf1ffabb9b7b739a6d5d
Go Yoshino <go.yoshino.2@gmail.com>
2022-07-27 10:47:13 +0900

    ユーティリティを更新

--- tests/util/test_ul_path.py
+++ tests/util/test_ul_path.py
@@ -6,31 +6,34 @@ class UnderrailPathTest(unittest.TestCase):
 
     def test_for_underrail_path_given(self):
 
-        json_path = r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg"
+        underrail_path = r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg"
 
         underrail_root = r"C:\SteamLibrary\steamapps\common\Underrail\data"
-        json_root = r"C:\SteamLibrary\steamapps\common\Underrail\data"
+        json_root = r"D:\underrail_json\data"
 
-        path = UnderrailPath(json_path, underrail_root, json_root)
+        path = UnderrailPath(underrail_path, underrail_root, json_root)
 
-        self.assertEqual(r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg.json",
+        self.assertEqual(r"D:\underrail_json\data\dialogs\events\cc_gorskyassaultcrawlers.udlg.json",
                          path.json(),)
         self.assertEqual(r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg",
                          path.datafile())
 
-    def test_for_json_path_given(self):
+    def dtest_for_json_path_given(self):
 
         underrail_path = r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg"
 
         underrail_root = r"C:\SteamLibrary\steamapps\common\Underrail\data"
-        json_root = r"C:\SteamLibrary\steamapps\common\Underrail\data"
+        json_root = r"D:\underrail_json\data"
+        patch_root = r"D:\patch\data"
 
-        path = UnderrailPath(underrail_path, underrail_root, json_root)
+        path = UnderrailPath(underrail_path, underrail_root, json_root, patch_root)
 
-        self.assertEqual(r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg.json",
+        self.assertEqual(r"D:\underrail_json\data\dialogs\events\cc_gorskyassaultcrawlers.udlg.json",
                          path.json(),)
         self.assertEqual(r"C:\SteamLibrary\steamapps\common\Underrail\data\dialogs\events\cc_gorskyassaultcrawlers.udlg",
                          path.datafile())
+        self.assertEqual(r"D:\patch\data\dialogs\events\cc_gorskyassaultcrawlers.udlg",
+                         path.patch())
 
 
 if __name__ == '__main__':
--- underrail_translation_kit/util/ul_path.py
+++ underrail_translation_kit/util/ul_path.py
@@ -4,18 +4,24 @@ from typing import Tuple
 
 class UnderrailPath:
 
-    def __init__(self, arg_path_str: str, underrail_root: str, json_root: str):
+    def __init__(self, arg_path_str: str, underrail_root: str, json_root: str, patch_root: str=None):
         """
-        実データのパスあるいはjsonディレクトリのパスを渡す。（どっちを渡したいケースもあるため）
-        どちらにせよ、"data"ディレクトリをルートとして認識する
-        :param arg_path: 実データあるいはjsonディレクトリのパス
+        まとめてファイルの書き換えやjsonの生成を行う際に便利なパスの変換を行う
+        "data"ディレクトリをルートとして認識する
+        :param arg_path_str: 実データのパスあるいはjsonディレクトリのパスを渡す。（どっちを渡したいケースもあるため）
+        :param underrail_root: underrailのdataディレクトリのパス
+        :param json_root: jsonのdataディレクトリのパス
+        :param patch_root: (Optional) パッチ保存先のdataディレクトリのパス
         """
+
         arg_path = Path(arg_path_str)
         arg_filename = arg_path.parts[-1:]
-        self.__underrail_filename = self.__get_unsuffixed_filename(arg_filename[0])
+        self.__unsuffixed_filename = self.__get_unsuffixed_filename(arg_filename[0])
         self.__relative_dir = self.__make_relative_dir(arg_path)
-        self.__underrail_data_dir = Path(underrail_root)
-        self.__json_data_dir = Path(json_root)
+
+        self.__underrail_data_root = Path(underrail_root)
+        self.__json_root = Path(json_root)
+        self.__patch_root = None if patch_root is None else Path(patch_root)
 
     def __get_unsuffixed_filename(self, arg_filename: str) -> str:
         if arg_filename[-5:] == ".json":    # given is json
@@ -25,13 +31,12 @@ class UnderrailPath:
 
     def __make_relative_dir(self, path: Path) -> Path:
         index = self.__get_data_directory_index(path)
-        return Path("/".join(path.parts[index:-1]))
+        return Path(os.sep.join(path.parts[index + 1:-1]))
 
     def __get_data_directory_index(self, path: Path):
-        index = 0
         for i, part in enumerate(path.parts):
             if part == "data":
-                return index
+                return i
         raise Exception(f'パスにdataディレクトリが含まれていません: {path}')
 
     def json(self) -> str:
@@ -39,11 +44,20 @@ class UnderrailPath:
         JSONの側のパスを返す
         :return: 文字列
         """
-        return os.path.join(str(self.__json_data_dir), str(self.__relative_dir) , str(self.__underrail_filename) + ".json")
+        return os.path.join(str(self.__json_root), str(self.__relative_dir), str(self.__unsuffixed_filename) + ".json")
 
     def datafile(self) -> str:
         """
         underrail dataの側のパスを返す
         :return: 文字列
         """
-        return os.path.join(str(self.__underrail_data_dir), str(self.__relative_dir), str(self.__underrail_filename))
+        return os.path.join(str(self.__underrail_data_root), str(self.__relative_dir), str(self.__unsuffixed_filename))
+
+    def patch(self):
+        """
+        patch出力先のパスを返す
+        :return: 文字列
+        """
+        if self.__patch_root is None:
+            raise Exception("patch data dirが登録されていません")
+        return os.path.join(str(self.__patch_root), str(self.__relative_dir), str(self.__unsuffixed_filename))
