869b45e6bd76f8d5765350ff977fc6a96439f21f
GoYoshino <go.yoshino.2@gmail.com>
2022-07-28 18:05:14 +0900

    updated postprocess

--- underrail_translation_kit/json_/postprocess.py
+++ underrail_translation_kit/json_/postprocess.py
@@ -8,19 +8,13 @@ def __postprocess(json_object: Dict[str, str]) -> bool:
     :return: False if the entry should be deleted
     """
 
-    # 1. avoid { className: "DM", value: "English" entries
-    if json_object["className"] == "DM" and json_object["originalText"] == "English":
+    # 1. avoid value: "English" entries spam
+    if json_object["originalText"] == "English":
         return False
 
     # 2. avoid elements suffixed with something like "(301db70e-7a5a-40db-a103-21e6927c1834)"
     if re.search(r"[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}", json_object["originalText"]):
         return False
-
-    # 3. give annotations for item
-    if json_object["className"] == "I:N":
-        json_object["description"] = "Item Name"
-    elif json_object["className"] == "I:D":
-        json_object["description"] = "Item Description"
 
     return True
 
