5625d3e8c71b7e2345ef89c32909340931ef5e50
GoYoshino <go.yoshino.2@gmail.com>
2022-07-28 15:36:07 +0900

    ParseResultをインターフェースに対応

--- underrail_translation_kit/msnrbf_parser/parse_result.py
+++ underrail_translation_kit/msnrbf_parser/parse_result.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 
-from underrail_translation_kit.msnrbf_parser.class_with_values import ClassWithValues
+from underrail_translation_kit.msnrbf_parser.record_with_values import RecordWithValues
 from .serialized_object import SerializedObject
 from .serialized_object_array import SerializedObjectArray
 
@@ -13,15 +13,15 @@ class ParseResult(SerializedObjectArray):
         super().__init__(items)
         self.__dictionary = self.__generate_record_with_values_dictionary()
 
-    def __generate_record_with_values_dictionary(self):
+    def __generate_record_with_values_dictionary(self) -> Dict[int, RecordWithValues]:
         dictionary = {}
         for item in self.items:
-            if isinstance(item, ClassWithValues):
+            if isinstance(item, RecordWithValues):
                 dictionary[item.get_object_id()] = item
 
         return dictionary
 
-    def __find_target_class(self, object_id: int) -> ClassWithValues:
+    def __find_target_record(self, object_id: int) -> RecordWithValues:
         target_class = None
         classes = self.get_member_class_dict()
         for key in classes:
@@ -32,7 +32,7 @@ class ParseResult(SerializedObjectArray):
         raise Exception(f"could not find a class with id={object_id}")
 
     def get_text(self, object_id: int) -> str:
-        target_class = self.__find_target_class(object_id)
+        target_class = self.__find_target_record(object_id)
         return target_class.get_text(object_id)
 
     def replace_text(self, new_text: str, object_id: int) -> None:
@@ -42,10 +42,10 @@ class ParseResult(SerializedObjectArray):
         :param object_id: object ID
         """
 
-        target_class = self.__find_target_class(object_id)
+        target_class = self.__find_target_record(object_id)
         target_class.replace_text(new_text, object_id)
 
         self.recalc_raw_bytes()
 
-    def get_member_class_dict(self) -> Dict[int, ClassWithValues]:
+    def get_member_class_dict(self) -> Dict[int, RecordWithValues]:
         return self.__dictionary
