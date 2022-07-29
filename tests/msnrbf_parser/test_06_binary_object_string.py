b1ea3fa8e0935a9c6e44e95722bf214240b1fa24
GoYoshino <go.yoshino.2@gmail.com>
2022-07-29 14:27:28 +0900

    added binary object string test

--- tests/msnrbf_parser/test_06_binary_object_string.py
+++ tests/msnrbf_parser/test_06_binary_object_string.py
@@ -1,5 +1,6 @@
 import unittest
 
+from underrail_translation_kit.msnrbf_parser.length_prefixed_string import LengthPrefixedString
 from underrail_translation_kit.msnrbf_parser.primitives import Int8
 from underrail_translation_kit.msnrbf_parser.binary_object_string import BinaryObjectString
 from .helper import assertEndOfStream, assertEqualToStream
@@ -16,6 +17,13 @@ class BinaryObjectStringTest(unittest.TestCase):
 
             assertEndOfStream(self, stream)
             assertEqualToStream(self, obj.raw_bytes, stream)
+
+    def test_direct_generation(self):
+        knickknack = "abccddeeeeefff"
+        subject = BinaryObjectString.from_params(455, knickknack)
+        self.assertEqual(455, subject.get_object_id())
+        self.assertEqual(knickknack, subject.get_string())
+        self.assertEqual(b"\x06" + (455).to_bytes(4, "little") + LengthPrefixedString.from_value(knickknack).raw_bytes, subject.raw_bytes)
 
 if __name__ == '__main__':
     unittest.main()
